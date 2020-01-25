a5cd1e60ffe98642c56b05ba9379bf36f24aa4cc
Sterling Watts 🍷 <SterlingDWatts@gmail.com>
2020-01-25 13:41:28 -0800

    Create 'types' endpoint and middleware

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-
-console.log(process.env.API_TOKEN)
+const POKEDEX = require('./pokedex.json');
 
 const app = express();
 
 app.use(morgan('dev'))
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization');
+
+    console.log('validate bearer token middleware')
+    
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+
+    next()
+})
 
 const validTypes = ['Bug', 'Dark', 'Dragon', 'Electric', 'Fairy', 'Flying', 'Ghost', 'Grass', 'Ground', 'Ice', 'Normal', 'Poison', 'Psychic', 'Rock', 'Steel', 'Water'];
 
@@ -15,6 +27,12 @@ function handleGetTypes(req, res) {
 }
 
 app.get('/types', handleGetTypes)
+
+function handleGetPokemon(req, res) {
+    res.send('Hello, Pokemon!');
+}
+
+app.get('/pokemon', handleGetPokemon)
 
 const PORT = 8000
 
